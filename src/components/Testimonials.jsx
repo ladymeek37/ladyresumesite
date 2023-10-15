1a7c2e3ecf381c5ac234c24c8e8c03145dc929b9
Lady Meek <ladyjmeek@gmail.com>
2023-10-15 12:01:31 -0700

    added amelias testimonial

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -15,7 +15,13 @@ const TestimonialsSection = () => {
                     </div>
                     <div>
                         <p className="testimonialtext">“Lady created a super cool website for me. She did it in a timely fashion, and added a lot of features that I really like. I would highly recommend her to anyone." - Joel, Owner of CRLW</p>
-                    </div>                    
+                    </div>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <div>
+                        <p className="testimonialtext">"Lady Meek is a very talented web designer. She created multiple new pages for our organization's website complete with graphics, a mobile-friendly layout and sub-menus. She is very responsive to edits and is very quick to make updates. She remains easy to reach and access and makes herself available. She is sure to get full approval before publishing changes. Lady is great to work with!" - Ameila Brodka, Co-Founder of Exposure </p>
+                    </div>                       
                 </div>
                 <hr/>
 
